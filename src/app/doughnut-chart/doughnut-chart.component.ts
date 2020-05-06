ae4dd58cfa91d397cb224caaa6eb98ef068bdfc9
moriyamasaaaki <moriya-7071@outlook.com>
2020-05-07 01:21:16 +0900

    feat(doughnut): ドーナツグラフ作成 #2

---
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-doughnut-chart',
+  templateUrl: './doughnut-chart.component.html',
+  styleUrls: ['./doughnut-chart.component.scss']
+})
+export class DoughnutChartComponent implements OnInit {
+
+  constructor() { }
+  doughnutChartLabels: Label[] = ['BMW', 'Ford', 'Tesla'];
+  doughnutChartData: MultiDataSet = [
+    [55, 25, 20]
+  ];
+  doughnutChartType: ChartType = 'doughnut';
+
+  ngOnInit() {
+  }
+
+}
